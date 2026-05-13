b65b0514e08079253793c7d0819914118d67fbc4
jakemsr <jakemsr@yahoo.com>
2026-05-13 04:01:54 -0700

    - initially set the array that will hold the Top Sellers   data to an array with falsy values - make Top Sellers data dynamic by pulling it from an API   and storing it in an array - when mapping the array of Top Sellers data to HTML/React,   if values are falsy,  display the Skeleton component from   UI/Skeleton to make a loading state

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
+import Skeleton from "../UI/Skeleton";
 
 const TopSellers = () => {
+
+  // falsy array to set up loading state
+  const [topSellers, setTopSellers] = useState(new Array(12).fill(0));
+  // to trigger re-render
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getTopSellers() {
+      try {
+        const response = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers");
+        setTopSellers(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getTopSellers();
+  }, []);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,21 +35,37 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
+              {topSellers.map((item, index) => (
                 <li key={index}>
                   <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
+                    <Link to={`/author/${item.authorId}`}>
+                      {!item.authorImage ? (
+                        <Skeleton width="50px" height="50px" borderRadius="50%" />
+                      ) : (
+                        <img
+                          className="lazy pp-author"
+                          src={item.authorImage}
+                          alt=""
+                        />
+                      )}
                       <i className="fa fa-check"></i>
                     </Link>
                   </div>
                   <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
+                    {!item.authorName ? (
+                      <Skeleton width="100px" height="20px" />
+                    ) : (
+                      <Link to={`/author/${item.authorId}`}>{item.authorName}</Link>
+                    )}
+                    <span>
+                      {!item.price ? (
+                        <Skeleton width="40px" height="20px" />
+                      ) : (
+                        <>
+                          {item.price} ETH
+                        </>
+                      )}
+                    </span>
                   </div>
                 </li>
               ))}
