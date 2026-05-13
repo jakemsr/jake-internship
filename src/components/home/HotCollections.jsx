45a912b761a01214f5cbedf5b49b700cfe7f5550
jakemsr <jakemsr@yahoo.com>
2026-05-12 21:26:11 -0700

    - Instead of hard coded, single, repeated entry for Hot Collections,   get the Hot Collections data from a backend with axios. - Display the Hot Collections data in a carousel made with   react-owl-carousel. - Display a loading state in the carousel while Hot Collections   data is being fetched. - Comment out the height property for the `nft_wrap` CSS class.   The hardcoded height was causing inconsistent visual effects   because it is a wrapper for images that are responsively sized   as the carousel scales. The example live site has the size   property set to 200%. Since the parent of `nft_wrap` does not   have a specific size, the 200% does nothing and the sizing falls   back to auto, just like if no size specification was given. - Remove useEffect callback from Home.jsx that scrolls to the top   on refresh. The example live site does not do that and the   behaviour is uncommon and annoying.

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import HotCollectionsCarousel from "./HotCollectionsCarousel";
 
 const HotCollections = () => {
+
+  // By default, fill `collection` with an array of falsy data
+  // with more than 4 items. This sets up the carousel loading state.
+  const [collection, setCollection] = useState(new Array(6).fill(0));
+
+  // The value of `loading` is not actually used. Without this and
+  // the setLoading(false) call in getCollection(), the carousel
+  // never leaves loading state. Console logging shows
+  // HotCollectionsCarousel() does run again after the axios
+  // response with `items` prop filled with correct data, but the
+  // carousel never re-renders.
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getCollection() {
+      try {
+        const response = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections');
+        setCollection(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getCollection();
+  }, []);
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,29 +39,7 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <span>ERC-192</span>
-                </div>
-              </div>
-            </div>
-          ))}
+          <HotCollectionsCarousel items={collection} />
         </div>
       </div>
     </section>
---
+++ src/components/home/HotCollectionsCarousel.jsx
@@ -0,0 +1,74 @@
+import { Link } from "react-router-dom";
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+
+const HotCollectionsCarousel = ({ items }) => {
+  const options = {
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1, // 1 slide for mobile (0px and up)
+      },
+      600: {
+        items: 2, // 2 slides for tablets (600px and up)
+      },
+      900: {
+        items: 3, // 3 slides for desktops (900px and up)
+      },
+      1200: {
+        items: 4, // never more than 4 items
+      },
+    },
+  };
+  return (
+    <OwlCarousel {...options}>
+      {items.map((item, index) => (
+        <div key={index}>
+          <div className="nft_coll">
+            <div className="nft_wrap">
+              {!item.nftImage ? (
+                <div className="skeleton-box img-fluid" style={{ width: "400px", height: "200px" }} />
+              ) : (
+                <Link to="/item-details">
+                  <img src={item.nftImage} className="lazy img-fluid" alt={item.title} />
+                </Link>
+              )}
+            </div>
+            <div className="nft_coll_pp">
+              {!item.authorImage ? (
+                <div className="skeleton-box pp-coll" style={{ width: "60px", height: "60px", borderRadius: "50%" }} />
+              ) : (
+                <Link to="/author">
+                  <img className="lazy pp-coll" src={item.authorImage} alt="" />
+                </Link>
+              )}
+              <i className="fa fa-check"></i>
+            </div>
+            <div className="nft_coll_info">
+              {!item.title ? (
+                <>
+                <div className="skeleton-box" style={{ width: "120px", height: "20px", paddingBottom: "5px" }} />
+                <br />
+                </>
+              ) : (
+                <Link to="/explore">
+                  <h4>{item.title}</h4>
+                </Link>
+              )}
+              {!item.code ? (
+                <div className="skeleton-box" style={{ width: "80px", height: "20px" }} />
+              ) : (
+                <span>ERC-{item.code}</span>
+              )}
+            </div>
+          </div>
+        </div>
+      ))}
+    </OwlCarousel>
+  );
+};
+
+export default HotCollectionsCarousel;
