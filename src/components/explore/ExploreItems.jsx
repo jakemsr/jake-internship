dbb004b8eeed08e3974e7dbf5f82227f53499981
jakemsr <jakemsr@yahoo.com>
2026-05-13 21:32:39 -0700

    - Implement Add More button with numberToDisplay state variable that   is incremented by 4 each time the Add More button is clicked. If   all items are displayed, no longer show the Add More button. - Implement item filter by using the API's filter option. Move   getItems() out of useEffect hook since it will be called each time   the user selects a new filter.

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -5,40 +5,48 @@ import ItemCard from "../UI/ItemCard";
 
 const ExploreItems = () => {
 
+  // falsy array to set up loading state
   const [items, setItems] = useState(new Array(16).fill(0));
+  // to trigger re-render
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    async function getItems() {
-      try {
-        const response = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/explore");
-        setItems(response.data);
-        setLoading(false);
-      } catch (error) {
-        console.error(error);
-      }
+  const [numberToDisplay, setNumberToDisplay] = useState(8);
+
+  async function getItems(filter) {
+    try {
+      const filterText = filter ? `?filter=${filter}` : "";
+      const response = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/explore${filterText}`);
+      setItems(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
     }
+  }
+
+  useEffect(() => {
     getItems();
   }, []);
 
   return (
     <>
       <div>
-        <select id="filter-items" defaultValue="">
+        <select id="filter-items" defaultValue="" onChange={(e) => getItems(e.target.value)}>
           <option value="">Default</option>
           <option value="price_low_to_high">Price, Low to High</option>
           <option value="price_high_to_low">Price, High to Low</option>
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {items.slice(0,8).map((item, index) => (
+      {items.slice(0, numberToDisplay).map((item, index) => (
         <ItemCard key={index} item={item} index={index} page="explore" />
       ))}
+      {numberToDisplay < items.length && (
       <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
+        <Link to="" id="loadmore" className="btn-main lead" onClick={() => setNumberToDisplay(numberToDisplay + 4)}>
           Load more
         </Link>
       </div>
+      )}
     </>
   );
 };
